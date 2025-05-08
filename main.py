218ba230015f0b7d2e65fcf6c8b50768c2b12e67
7heice <rynadalsdrany@gmail.com>
2025-05-08 23:45:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import time
 load_dotenv()
 
 # إعداد مفاتيح تويتر
-api_key = os.getenv("TWITTER_API_KEY")
-api_secret = os.getenv("TWITTER_API_SECRET")
-access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-access_secret = os.getenv("TWITTER_ACCESS_SECRET")
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_secret = os.getenv("ACCESS_SECRET")
 
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_secret)
 api = tweepy.API(auth)
